0547f245b7235e129f04c80250cfbd7771f0f784
M.K.D. Githmi Thathsarani <mkdgthathsarani@gmail.com>
2026-09-23 23:09:43 +0530

    Add today's SQL notes

--- Recap/Day_01/RecapDay01.sql
+++ Recap/Day_01/RecapDay01.sql
@@ -8,3 +8,17 @@ create table Students(
 	City VARCHAR(50)
 )
 
+create table Courses(
+	CourseID INT primary key,
+	CourseName VARCHAR(100),
+	Credits INT
+)
+
+select * from Students;
+desc Students;
+
+alter table Students add Emaill VARCHAR(50);
+
+alter table Students modify age INT;
+
+drop table Courses;
